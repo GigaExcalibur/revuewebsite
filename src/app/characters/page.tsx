237b37fdf295e2cdd20346fe3fdf7effbe82184b
GigaExcalibur <jlevitt8962@gmail.com>
2023-12-20 22:17:42 -0800

    miscellaneous stuff

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -4,16 +4,32 @@ import { IRenderCharacterConfig, UnitSheet, IUnit} from "@dream-of-components/re
 import { RoDCharacterArray } from "@rod/config"
 import { RoDRenderCharacter } from "@rod/config"
 import { RoDArtistConfig } from "@rod/config"
+import { useState } from "react";
+import CharacterDetails from "@rod/components/characterDetails";
+
+let characters: RoDRenderCharacter[];
+let spritesheet: { [key: string]: IRenderItemConfig[] } = {};
+let init = false;
 
 export default function characterPage() {
 
-	const characters = RoDCharacterArray.map((character: IUnit) => new RoDRenderCharacter(character, {renderAll: true}));
-	const spritesheet = {
-		player: characters.map((character: RoDRenderCharacter) => {
-		character.currentChapter = {chapter: Number.MAX_SAFE_INTEGER}; // this line will actually trigger the code to parse sh it
-		return character.data; // this line gets the parsed 
-	})
-	};
+	if(!init){
+	  characters = RoDCharacterArray.map((character: IUnit) => new RoDRenderCharacter(character, {renderAll: true}));
+	  spritesheet = {
+			player: characters.map((character: RoDRenderCharacter) => {
+			character.currentChapter = {chapter: Number.MAX_SAFE_INTEGER}; // this line will actually trigger the code to parse sh it
+			return character.data; // this line gets the parsed 
+		})
+		};
+	  init = true;
+	}
+	
+	let activeCharacter: any; 
+
+	const [characterPageState, updateCharacterPage] = useState({
+			spritesheet,
+			activeCharacter
+		});
 
 	const artistConfig = {};
 	const expandedPortraits = new Map<string, boolean>();
@@ -24,14 +40,23 @@ export default function characterPage() {
 	
 	function onCharacterClick(characterData: any) {
 		return () => {
-		
-		}
+                updateCharacterPage({ ...characterPageState, activeCharacter: characterData });
+            }
 	};
+	
+    function clearCharacter() {
+        updateCharacterPage({ ...characterPageState, activeCharacter: undefined });
+    }
 
-	return <UnitSheet
-        data={spritesheet}
-		expansionState={expandedPortraits}
-        toggleCharacter={toggleCharacter}
-		getOnClick={onCharacterClick} 
-        artistConfig={RoDArtistConfig} />;
+	return <>
+	{characterPageState.activeCharacter ? <CharacterDetails
+            characterConfig={characterPageState.activeCharacter}
+            clear={clearCharacter} /> : ''}
+	<UnitSheet
+			data={characterPageState.spritesheet}
+			expansionState={expandedPortraits}
+			toggleCharacter={toggleCharacter}
+			getOnClick={onCharacterClick} 
+			artistConfig={RoDArtistConfig} />
+	</>
 }
---
+++ src/components/characterDetails/index.tsx
@@ -0,0 +1,48 @@
+import { IUnit } from "@dream-of-components/react"
+import { CharacterOverlay } from "@dream-of-components/react";
+
+export default function CharacterDetails({characterConfig, clear}: {
+    characterConfig: IRenderCharacterConfig,
+    clear: () => void
+}) {
+	const defaultView = 'test';
+	const validViews = new Set('test');
+	
+	const characterDef: IUnit = characterConfig.unitData;
+
+	const uiIcons = {
+		dragDrop: () => 'drag',
+		exit: () => 'x',
+		removeBlossom: () => '-',
+		addBlossom: (isAtLimit: boolean) => '+'
+	};
+
+		function renderSideProfile() {
+			  return <div>
+			    <img src={characterDef.path} />
+				<p>{characterDef.displayName}</p>
+				<p className="font-sm">{characterDef.description}</p>
+			  </div>
+			}
+
+		function renderContent(state: string) {
+			return <div>
+				<p>{characterDef.class}</p>
+				<p className="font-sm">{characterDef.classDesc}</p>
+			  </div>
+		}
+
+		function renderTabs(currentState: string, onTabSelect: (selectedState: string) => void) {
+			return '';
+		}
+	return <CharacterOverlay
+			clear={clear}
+			renderContent={renderContent}
+			renderTabs={renderTabs}
+			renderSideProfile={renderSideProfile}
+			initialState={defaultView!}
+			validViews={validViews}
+			uiIcons={uiIcons}
+		/>
+}
+
